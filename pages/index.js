a780a353e9617a03306c7047232a35e5cefc9b46
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-04-13 09:08:04 -0700

    typo

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ posts }) {
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-22SJJZ91C2"
         strategy="afterInteractive" />
 
-      <Script id="google-analytics" strategy="afterInteractive>
+      <Script id="google-analytics" strategy="afterInteractive" >
         {`
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
@@ -36,22 +36,22 @@ export default function Home({ posts }) {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 px-3 md:px-8 ">
-      <div className='lg:col-span-12 col-span-1'>
-        {/* {posts.map((post) => (
+        <div className='lg:col-span-12 col-span-1'>
+          {/* {posts.map((post) => (
             <PostCard post={post.node} key={post.title} />
           ))} */}
-        {/* <FeaturedPosts /> */}
+          {/* <FeaturedPosts /> */}
 
-      </div>
+        </div>
 
-      {/* <div className="lg:col-span-4 col-span-1">
+        {/* <div className="lg:col-span-4 col-span-1">
           <div className='lg:sticky relative top-8'>
             <PostWidget />
             <Categories />
           </div>
 
         </div> */}
-    </div>
+      </div>
     </div > //closing div
 
   );
